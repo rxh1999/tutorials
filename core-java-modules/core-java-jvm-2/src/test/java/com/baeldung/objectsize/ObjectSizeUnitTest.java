8e0a7c533a9079074f364ec121ab5e4397b085a2
renxiaohui <renxiaohui99@gmail.com>
2022-10-24 23:33:31 +0800

    calculate nested map size

--- core-java-modules/core-java-jvm-2/src/test/java/com/baeldung/objectsize/ObjectSizeUnitTest.java
+++ core-java-modules/core-java-jvm-2/src/test/java/com/baeldung/objectsize/ObjectSizeUnitTest.java
@@ -5,7 +5,57 @@ import org.openjdk.jol.info.ClassLayout;
 import org.openjdk.jol.info.GraphLayout;
 import org.openjdk.jol.vm.VM;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ObjectSizeUnitTest {
+
+    @Test
+    public void nestedMap(){
+        Map nestedMap = new HashMap();
+        List<String> typeKey = new ArrayList<>();
+        typeKey.add("TYPE1");
+        typeKey.add("TYPE2");
+        typeKey.add("TYPE3");
+        List<String> portKey = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            portKey.add("PORT"+i);
+        }
+        add(nestedMap, typeKey, portKey, new Object(), 0);
+        long total = GraphLayout.parseInstance(nestedMap).totalSize();
+        System.out.println("3*100*100*100*1 nested map: " + total + "B");
+
+        int l0 = 0;
+        int l1 = 3 * 1;
+        int l2 = 3 * 100 * 1;
+        int l3 = 3 * 100 * 100 * 1;
+        long perStringSize = GraphLayout.parseInstance("PORT0").totalSize();
+        long overCal = ((l0+l1+l2+l3 - 1) * 100 * perStringSize);
+        System.out.println("per port string size: " + perStringSize);
+        System.out.println("all over calculate string size: " + overCal);
+        System.out.println("actually nested map size: " + (total - overCal) + "B");
+    }
+
+    private void add(Map nestedMap, List<String> typeKey, List<String> portKey, Object o, int idx) {
+        List<String> keyList = new ArrayList<>();
+        if (idx == 0){
+            keyList = typeKey;
+        }else if (idx >0 && idx <3){
+            keyList = portKey;
+        }
+        if (idx == 3){
+            for (String key : portKey) {
+                ((List)nestedMap.computeIfAbsent(key.intern(), k->new ArrayList<>())).add(o);
+            }
+        }else{
+            for (String key : keyList) {
+                Map subMap = (Map) nestedMap.computeIfAbsent(key.intern(), k->new HashMap<>());
+                add(subMap, typeKey, portKey, o, idx + 1);
+            }
+        }
+    }
 
     @Test
     public void printingTheVMDetails() {
